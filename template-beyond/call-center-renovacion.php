f91f285fe3bc3c3e2063d9a817018ccc4e6f59d5
Esmeralda <ifglitterthen@gmail.com>
2021-08-26 18:42:06 -0500

    final update

--- template-beyond/call-center-renovacion.php
+++ template-beyond/call-center-renovacion.php
@@ -1,11 +1,27 @@
 <ul class="nav nav-pills level-four" id="beyond-renovacion-callcenter" role="tablist">
     <li class="nav-item" role="presentation">
-        <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
-            aria-controls="pills-home" aria-selected="true">Base de datos (Llamadas)</a>
+        <a class="nav-link  active" id="kpis-info-beyond-renovacion-tab" data-toggle="pill"
+            href="#kpis-info-beyond-renovacion" role="tab" aria-controls="kpis-info-beyond-renovacion"
+            aria-selected="true">KPI's de telefonía</a>
+    </li>
+
+    <li class="nav-item" role="presentation">
+        <a class="nav-link" id="call-base de datos-info-renovacion-tab" data-toggle="pill"
+            href="#call-base de datos-info-renovacion" role="tab" aria-controls="call-base de datos-info-renovacion"
+            aria-selected="false">Base de datos (Llamadas)</a>
     </li>
 </ul>
 <div class="tab-content" id="beyond-renovacion-callcenterContent">
-    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+    <div class="tab-pane fade show active" id="kpis-info-beyond-renovacion" role="tabpanel"
+        aria-labelledby="kpis-info-beyond-renovacion-tab">
+
+    </div>
+    <div class="tab-pane fade" id="call-base de datos-info-renovacion" role="tabpanel"
+        aria-labelledby="call-base de datos-info-renovacion-tab">
         <?php include_once('./template-beyond/call-base-de-datos-renovacion.php') ?>
     </div>
-</div>   
+    <div class="tab-pane fade show active" id="kpis-info-beyond-renovacion" role="tabpanel"
+        aria-labelledby="kpis-info-beyond-renovacion-tab">
+        <?php include_once('./template-beyond/kpis-info-renovacion.php') ?>
+    </div>
+</div>
--- template-beyond/call-center.php
+++ template-beyond/call-center.php
@@ -1,13 +1,21 @@
 <ul class="nav nav-pills level-four" id="beyond-cobranza-callcenter-base-datos" role="tablist">
-    <li class="nav-item table_llamadas" role="presentation">
-        <a class="nav-link active" id="beyond-cobranza-callcenter-bd-tab" data-toggle="pill"
-            href="#beyond-cobranza-callcenter-bd" role="tab" aria-controls="beyond-cobranza-callcenter-bd"
-            aria-selected="true">Base de datos (Llamadas)</a>
+    <li class="nav-item" role="presentation">
+        <a class="nav-link  active" id="kpis-info-beyond-cobranza-tab" data-toggle="pill"
+            href="#kpis-info-beyond-cobranza" role="tab" aria-controls="kpis-info-beyond-cobranza"
+            aria-selected="true">KPI's de telefonía</a>
     </li>
-
+    <li class="nav-item table_llamadas" role="presentation">
+        <a class="nav-link" id="beyond-cobranza-callcenter-bd-tab" data-toggle="pill"
+            href="#beyond-cobranza-callcenter-bd" role="tab" aria-controls="beyond-cobranza-callcenter-bd"
+            aria-selected="false">Base de datos (Llamadas)</a>
+    </li>
 </ul>
 <div class="tab-content" id="beyond-cobranza-callcenter-base-datosContent">
-    <div class="tab-pane fade show active" id="beyond-cobranza-callcenter-bd" role="tabpanel"
+    <div class="tab-pane fade show active" id="kpis-info-beyond-cobranza" role="tabpanel"
+        aria-labelledby="kpis-info-beyond-cobranza-tab">
+        <?php include_once('./template-beyond/kpis-info.php') ?>
+    </div>
+    <div class="tab-pane fade" id="beyond-cobranza-callcenter-bd" role="tabpanel"
         aria-labelledby="beyond-cobranza-callcenter-bd-tab">
         <?php include_once('./template-beyond/call-base-de-datos.php') ?>
     </div>
