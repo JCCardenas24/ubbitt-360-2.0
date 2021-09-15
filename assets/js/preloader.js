8c6b5773e6494d1cb525089125221fa9554339a5
Esmeralda <ifglitterthen@gmail.com>
2021-09-15 16:57:25 -0500

    preload component

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -2,7 +2,7 @@ $('#premium-call-center-bd-llamadas-campaign-1-content-tab').on('shown.bs.tab', 
     event.target // newly activated tab
     event.relatedTarget // previous active tab
     $('#loading_content').modal('show')
-        // setTimeout(function() {
-        //     $("#loading_content").modal("hide");
-        // }, 5000);
+    setTimeout(function() {
+        $("#loading_content").modal("hide");
+    }, 5000);
 })
