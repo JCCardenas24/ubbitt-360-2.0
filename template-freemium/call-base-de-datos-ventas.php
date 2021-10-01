6cba7e475515da527081774eb20a25d1c5abb5c5
Esmeralda <ifglitterthen@gmail.com>
2021-10-01 11:21:32 -0500

    vista/ base de datos (ventas)

--- template-freemium/call-base-de-datos-ventas.php
+++ template-freemium/call-base-de-datos-ventas.php
@@ -75,18 +75,28 @@
       <td>$0.00</td>
       <td><a href="./assets/reportes/prueba.xlsx" download><i class="fa fa-download" aria-hidden="true"></i></a></td>
     </tr>
-    
+
   </tbody>
 </table>
 
-<nav aria-label="Page navigation example">
-  <ul class="pagination justify-content-end">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item disabled">
-      <a class="page-link " href="#">Siguiente</a>
-    </li>
-  </ul>
-</nav>
+<div class="d-flex footer_table">
+  <small>Mostrando 1 al 10 de 12 registros</small>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-end">
+      <li class="page-item disabled">
+        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
+      </li>
+      <!-- <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li> -->
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item active" aria-current="page">
+        <span class="page-link">2</span>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item">
+      <li class="page-item disabled">
+        <a class="page-link " href="#">Siguiente</a>
+      </li>
+    </ul>
+  </nav>
+</div>
