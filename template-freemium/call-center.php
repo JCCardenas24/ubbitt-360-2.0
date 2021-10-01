83fb7391e5eab27f37fe35e375134da577bc517b
Esmeralda <ifglitterthen@gmail.com>
2021-10-01 09:54:27 -0500

    vista/ base de datos (ventas)

--- template-freemium/call-center.php
+++ template-freemium/call-center.php
@@ -6,7 +6,11 @@
     </li>
     <li class="nav-item table_llamadas" role="presentation">
         <a class="nav-link" id="freemium-call-center-bd-tab" data-toggle="pill" href="#freemium-call-center-bd"
-            role="tab" aria-controls="freemium-call-center-bd" aria-selected="false">Base de Datos</a>
+            role="tab" aria-controls="freemium-call-center-bd" aria-selected="false">Base de datos (Llamadas)</a>
+    </li>
+    <li class="nav-item table_llamadas" role="presentation">
+        <a class="nav-link" id="freemium-call-center-ventas-tab" data-toggle="pill" href="#freemium-call-center-ventas"
+            role="tab" aria-controls="freemium-call-center-ventas" aria-selected="false">Base de datos (Ventas)</a>
     </li>
 </ul>
 <div class="tab-content" id="freemium-call-center-optionsContent">
@@ -17,5 +21,9 @@
     <div class="tab-pane fade" id="freemium-call-center-bd" role="tabpanel"
         aria-labelledby="freemium-call-center-bd-tab">
         <?php include_once('call-base-de-datos.php')?>
+    </div>
+    <div class="tab-pane fade" id="freemium-call-center-ventas" role="tabpanel"
+        aria-labelledby="freemium-call-center-ventas-tab">
+        <?php include_once('call-base-de-datos-ventas.php')?>
     </div>
 </div>
