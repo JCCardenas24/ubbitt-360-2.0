93b5beac7498996cf8680b93c673669011b55373
Esmeralda <ifglitterthen@gmail.com>
2021-11-03 16:17:39 -0600

    update

--- template-freemium/reportes.php
+++ template-freemium/reportes.php
@@ -1,5 +1,5 @@
 <div class="row main_row_reportes_ttl ml-0 mr-0">
-    <h1>Mes actual</h1>
+    <h1>Mes actual <span>Base entregada 138</span></h1>
     <div class="d-flex">
         <div class="form-group mb-0">
             <select class="form-control" id="exampleFormControlSelect1">
@@ -11,54 +11,50 @@
         </a>
     </div>
 </div>
-<div class="table-responsive">
+<div class="table-responsive table_double_fixed_column">
     <table class="table table-hover">
         <thead>
             <tr>
-                <th scope="col">KPIS</th>
-                <th scope="col">Monthy</th>
-                <th colspan="31">Productividad diaria</th>
+                <th scope="col" class="fix_column first">KPIS</th>
+                <th scope="col" class="fix_column second">Monthy</th>
+                <th scope="col" class="days">1</th>
+                <th scope="col" class="days">2</th>
+                <th scope="col" class="days">3</th>
+                <th scope="col" class="days">4</th>
+                <th scope="col" class="days">5</th>
+                <th scope="col" class="days">6</th>
+                <th scope="col" class="days">7</th>
+                <th scope="col" class="days">8</th>
+                <th scope="col" class="days">9</th>
+                <th scope="col" class="days">10</th>
+                <th scope="col" class="days">11</th>
+                <th scope="col" class="days">12</th>
+                <th scope="col" class="days">13</th>
+                <th scope="col" class="days">14</th>
+                <th scope="col" class="days">15</th>
+                <th scope="col" class="days">16</th>
+                <th scope="col" class="days">17</th>
+                <th scope="col" class="days">18</th>
+                <th scope="col" class="days">19</th>
+                <th scope="col" class="days">20</th>
+                <th scope="col" class="days">21</th>
+                <th scope="col" class="days">22</th>
+                <th scope="col" class="days">23</th>
+                <th scope="col" class="days">24</th>
+                <th scope="col" class="days">25</th>
+                <th scope="col" class="days">26</th>
+                <th scope="col" class="days">27</th>
+                <th scope="col" class="days">28</th>
+                <th scope="col" class="days">29</th>
+                <th scope="col" class="days">30</th>
+                <th scope="col" class="days">31</th>
             </tr>
-            <tr>
-                <th scope="col">Base entregada</th>
-                <th scope="col">138</th>
-                <th scope="col">1</th>
-                <th scope="col">2</th>
-                <th scope="col">3</th>
-                <th scope="col">4</th>
-                <th scope="col">5</th>
-                <th scope="col">6</th>
-                <th scope="col">7</th>
-                <th scope="col">8</th>
-                <th scope="col">9</th>
-                <th scope="col">10</th>
-                <th scope="col">11</th>
-                <th scope="col">12</th>
-                <th scope="col">13</th>
-                <th scope="col">14</th>
-                <th scope="col">15</th>
-                <th scope="col">16</th>
-                <th scope="col">17</th>
-                <th scope="col">18</th>
-                <th scope="col">19</th>
-                <th scope="col">20</th>
-                <th scope="col">21</th>
-                <th scope="col">22</th>
-                <th scope="col">23</th>
-                <th scope="col">24</th>
-                <th scope="col">25</th>
-                <th scope="col">26</th>
-                <th scope="col">27</th>
-                <th scope="col">28</th>
-                <th scope="col">29</th>
-                <th scope="col">30</th>
-                <th scope="col">31</th>
-            </tr>
+            
         </thead>
         <tbody>
             <tr>
-                <th scope="row">Llamadas realizadas</th>
-                <td>1409</td>
+                <th scope="row" class="fix_column first">Llamadas realizadas</th>
+                <th class="fix_column second">1409</th>
                 <td>200</td>
                 <td>600</td>
                 <td>200</td>
@@ -90,6 +86,146 @@
                 <td>55</td>
                 <td>22</td>
                 <td>22</td>
+            </tr>
+            <tr>
+                <th scope="col" class="fix_column first">Llamadas conectadas</th>
+                <th scope="col" class="fix_column second">424</th>
+                <td scope="col">54</td>
+                <td scope="col">54</td>
+                <td scope="col">0</td>
+                <td scope="col">47</td>
+                <td scope="col">61</td>
+                <td scope="col">56</td>
+                <td scope="col">42</td>
+                <td scope="col">35</td>
+                <td scope="col">39</td>
+                <td scope="col">0</td>
+                <td scope="col">54</td>
+                <td scope="col">0</td>
+                <td scope="col">53</td>
+                <td scope="col">44</td>
+                <td scope="col">55</td>
+                <td scope="col">56</td>
+                <td scope="col">57</td>
+                <td scope="col">58</td>
+                <td scope="col">59</td>
+                <td scope="col">50</td>
+                <td scope="col">41</td>
+                <td scope="col">52</td>
+                <td scope="col">53</td>
+                <td scope="col">44</td>
+                <td scope="col">45</td>
+                <td scope="col">56</td>
+                <td scope="col">47</td>
+                <td scope="col">48</td>
+                <td scope="col">59</td>
+                <td scope="col">50</td>
+                <td scope="col">51</td>
+            </tr>
+            <tr>
+                <th scope="col" class="fix_column first">Renovación aceptada</th>
+                <th scope="col" class="fix_column second">52</th>
+                <td scope="col">5</td>
+                <td scope="col">5</td>
+                <td scope="col">0</td>
+                <td scope="col">4</td>
+                <td scope="col">6</td>
+                <td scope="col">5</td>
+                <td scope="col">2</td>
+                <td scope="col">3</td>
+                <td scope="col">3</td>
+                <td scope="col">0</td>
+                <td scope="col">5</td>
+                <td scope="col">0</td>
+                <td scope="col">3</td>
+                <td scope="col">4</td>
+                <td scope="col">5</td>
+                <td scope="col">6</td>
+                <td scope="col">7</td>
+                <td scope="col">8</td>
+                <td scope="col">9</td>
+                <td scope="col">0</td>
+                <td scope="col">1</td>
+                <td scope="col">2</td>
+                <td scope="col">3</td>
+                <td scope="col">4</td>
+                <td scope="col">4</td>
+                <td scope="col">5</td>
+                <td scope="col">4</td>
+                <td scope="col">4</td>
+                <td scope="col">5</td>
+                <td scope="col">5</td>
+                <td scope="col">5</td>
+            </tr>
+            <tr>
+                <th scope="col" class="fix_column first">Conversión B12</th>
+                <th scope="col" class="fix_column second">5,2%</th>
+                <td scope="col">1.9%</td>
+                <td scope="col">0.0%</td>
+                <td scope="col">0.0%</td>
+                <td scope="col">6.4%</td>
+                <td scope="col">4.5%</td>
+                <td scope="col">5.6%</td>
+                <td scope="col">4.2%</td>
+                <td scope="col">3.5%</td>
+                <td scope="col">3.9%</td>
+                <td scope="col">1.9%</td>
+                <td scope="col">5.4%</td>
+                <td scope="col">3.2%</td>
+                <td scope="col">5.3%</td>
+                <td scope="col">4.4%</td>
+                <td scope="col">5.5%</td>
+                <td scope="col">5.6%</td>
+                <td scope="col">5.7%</td>
+                <td scope="col">5.8%</td>
+                <td scope="col">5.9%</td>
+                <td scope="col">5.0%</td>
+                <td scope="col">4.1%</td>
+                <td scope="col">5.2%</td>
+                <td scope="col">5.3%</td>
+                <td scope="col">4.4%</td>
+                <td scope="col">4.5%</td>
+                <td scope="col">5.6%</td>
+                <td scope="col">4.7%</td>
+                <td scope="col">4.8%</td>
+                <td scope="col">5.9%</td>
+                <td scope="col">5.0%</td>
+                <td scope="col">5.1%</td>
+            </tr>
+            <tr>
+                <th scope="col" class="fix_column first">Registros pagados</th>
+                <th scope="col" class="fix_column second">20</th>
+                <td scope="col">1.</td>
+                <td scope="col">0</td>
+                <td scope="col">0</td>
+                <td scope="col">6</td>
+                <td scope="col">4</td>
+                <td scope="col">5</td>
+                <td scope="col">4</td>
+                <td scope="col">3</td>
+                <td scope="col">9</td>
+                <td scope="col">9</td>
+                <td scope="col">4</td>
+                <td scope="col">3</td>
+                <td scope="col">3</td>
+                <td scope="col">4</td>
+                <td scope="col">5</td>
+                <td scope="col">5</td>
+                <td scope="col">5</td>
+                <td scope="col">8</td>
+                <td scope="col">9</td>
+                <td scope="col">5</td>
+                <td scope="col">4</td>
+                <td scope="col">2</td>
+                <td scope="col">3</td>
+                <td scope="col">4</td>
+                <td scope="col">5</td>
+                <td scope="col">6</td>
+                <td scope="col">7</td>
+                <td scope="col">8</td>
+                <td scope="col">9</td>
+                <td scope="col">5</td>
+                <td scope="col">5</td>
             </tr>
         </tbody>
     </table>
